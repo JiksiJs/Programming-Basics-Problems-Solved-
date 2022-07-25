7f7f681c049eaf0a20c322601df6bf8ff63ed8d9
JiksiJs <103967363+JiksiJs@users.noreply.github.com>
2022-07-25 19:54:28 +0100

    Add files via upload

---
+++ areaFigures.js
@@ -0,0 +1,17 @@
+function areaFigures(input) {
+    let figure = input[0];
+    let a = Number(input[1]);
+    let b = Number(input[2]);
+
+    if (figure === "square") {
+        console.log((a * a).toFixed(3));
+    } else if (figure === "rectangle") {
+        console.log((a * b).toFixed(3));
+    } else if (figure === "circle") {
+        console.log((Math.PI * a * a).toFixed(3));
+    } else if (figure === "triangle") {
+        console.log(((a * b) / 2).toFixed(3));
+    }
+}
+
+areaFigures(["square", "5"])
